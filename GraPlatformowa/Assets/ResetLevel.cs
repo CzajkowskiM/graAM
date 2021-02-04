a5a48dffa6afa193cb96885f00dbb79e4476e60c
CzajkowskiM <78494590+CzajkowskiM@users.noreply.github.com>
2021-02-04 15:15:32 +0100

    Menu

---
+++ GraPlatformowa/Assets/ResetLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResetLevel : MonoBehaviour
+{
+
+private Scene scene;
+
+void Start()
+{
+scene = SceneManager.GetActiveScene();
+}
+
+void OnTriggerEnter(Collider other)
+{
+if (other.gameObject.tag == "Player")
+	{
+	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+}
+
+void OnTriggerExit(Collider other) {
+SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+}
+
+
+}
