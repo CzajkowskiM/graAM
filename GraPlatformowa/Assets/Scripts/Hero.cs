cdb801d724fcf79b8d6eab1e1a645ac33916a2d9
Joanna Baran <jnnbaran@gmail.com>
2021-02-05 11:42:23 +0100

    player movement POC

---
+++ GraPlatformowa/Assets/Scripts/Hero.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    public float horizontalMove = 0f;
+     public bool jump = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+         
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        jump = false;
+     
+    }
+}
